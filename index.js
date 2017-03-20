d3fae1010d2ff7e5f24ee824d54e3fb596f2274e
jxom <jakemoxey@gmail.com>
2017-03-21 00:43:08 +1100

    Added a  query parameter

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const { send } = require('micro');
 const microApi = require('micro-api');
+const moment = require('moment');
 const YQL = require('yqlp');
 
 const BASE_URL = 'https://query.yahooapis.com/v1/public';
 const USAGE_TEXT = 'Usage:\n\n\nGET /?text=<suburb,town,city,state>\nE.g. /?text=Melbourne, Victoria\n\nGET /?lat=<latitude>&lng=<longitude>\nE.g. /?lat=-37&lng=145';
 
-const ymlSelectForecastQuery = (whereText) => `select location, wind, atmosphere, item.condition, item.forecast from weather.forecast where woeid in (select woeid from geo.places where text="${whereText}") LIMIT 1`;
+const ymlSelectForecastQuery = (whereText, offset = 0) => `select location, wind, atmosphere, item.condition, item.forecast from weather.forecast where woeid in (select woeid from geo.places where text="${whereText}") LIMIT 1 OFFSET ${offset}`;
 const fahrenheitToCelcius = (tempFahrenheit) => ((tempFahrenheit - 32) * (5/9));
 
 const parseResult = ({ channel: { location, wind, atmosphere, item } }) => ({
   location,
   current: {
+    date: item.condition.date,
     windSpeed: wind.speed,
     humidity: atmosphere.humidity,
     tempCelcius: fahrenheitToCelcius(item.condition.temp).toFixed(0),
     tempFahrenheit: item.condition.temp,
     condition: item.condition.text,
   },
-  today: {
+  day: {
+    date: item.forecast.date,
     low: {
       tempCelcius: fahrenheitToCelcius(item.forecast.low).toFixed(0),
       tempFahrenheit: item.forecast.low,
@@ -32,11 +35,24 @@ const parseResult = ({ channel: { location, wind, atmosphere, item } }) => ({
 
 const handleGetWeather = async ({ params, res }) => {
   try {
+    let recordOffset = 0;
+    let diff;
+    if (params.when && params.when !== 'today') {
+      if (params.when === 'tomorrow') {
+        recordOffset = 2;
+      } else if (moment(params.when, 'D/MM').isValid()) {
+        diff = moment(params.when, 'D/MM').diff(moment(), 'days');
+        recordOffset = diff + 2;
+      } else {
+        return send(res, 400, { error: 'Query parameter \'when\' accepts only: `today`, `tomorrow`, or `{D/MM}` (date)' });
+      }
+    }
+
     let yqlResp;
     if (params.text) {
-      yqlResp = await YQL.execp(ymlSelectForecastQuery(params.text));
+      yqlResp = await YQL.execp(ymlSelectForecastQuery(params.text, recordOffset));
     } else if (params.lat && params.lng) {
-      yqlResp = await YQL.execp(ymlSelectForecastQuery(`(${params.lat},${params.lng})`));
+      yqlResp = await YQL.execp(ymlSelectForecastQuery(`(${params.lat},${params.lng})`, recordOffset));
     } else {
       return USAGE_TEXT;
     }
@@ -44,18 +60,20 @@ const handleGetWeather = async ({ params, res }) => {
     if (yqlResp.query.count === 0) {
       return {};
     }
+
     return parseResult(yqlResp.query.results);
   } catch (e) {
-    return send(res, 500, 'Oops, we dun goof. An internal error occured. 😔🔫');
+    console.log(e);
+    return send(res, 500, { error: 'Oops, we dun goof. An internal error occured. 😔🔫' });
   }
-}
+};
 
 const api = microApi([
   {
     method: 'get',
     path: '/',
     handler: handleGetWeather,
-  },
+  }
 ]);
 
 module.exports = api;
